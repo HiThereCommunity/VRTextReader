9c211f1e0690e1d0ce5ea88789535ad98a621418
Anton Jan Rutten <antonjanrutten@outlook.com>
2017-05-20 13:05:28 +0200

    changes

---
+++ TextSplitter.vr.js
@@ -0,0 +1,21 @@
+import React from "react";
+//import textStyle from './textStyle.css';
+
+export default class TextSplitter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            leftPartOfWord: '',
+            rightPartOfWord: '',
+            highlightedLetter: ''
+        }
+        setWords();
+    }
+    
+    render() {
+        return (
+            
+
+        )
+    }
+}
--- index.vr.js
+++ index.vr.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { AppRegistry, asset, Pano, Text, View } from "react-vr";
 import text from "./text";
-
+import TextSplitter from './TextSplitter';
 const textSplit = text.split(" ");
 
 export default class VRTextReader extends React.Component {
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.timer = this.timer.bind(this);
-    this.state = {
-      currentCount: 0
+        this.timer = this.timer.bind(this);
+
+        this.state = {
+            currentCount: 0,
+            leftPartOfWord: '',
+            rightPartOfWord: '',
+            highlightedLetter: ''
+        }
     }
-  }
   componentDidMount() {
     var intervalId = setInterval(this.timer, 200);
     // store intervalId in the state so it can be accessed later:
@@ -26,34 +30,73 @@ export default class VRTextReader extends React.Component {
   }
 
   timer() {
-    // setState method is used to update the state
-
+      //if (this.state.currentCount > 10) {
+      //    clearInterval(this.state.intervalId);
+      //}
+    var txt = textSplit[this.state.currentCount];
+    var wordChars = Array.from(txt);
+    var wordCharsLength = wordChars.length;
+    var wordCharsLengthFirstPart = Math.floor(wordCharsLength / 2);
+    var leftPartWithoutHighlightedLetter = txt.substring(0, wordCharsLengthFirstPart - 1);
+    var highlightedLetter = txt.substring(leftPartWithoutHighlightedLetter.length, leftPartWithoutHighlightedLetter.length + 1);
+    var rightPart = txt.substring(leftPartWithoutHighlightedLetter.length + 1, txt.length);
+    
     if (this.state.currentCount === textSplit.length) {
       clearInterval(this.state.intervalId);
     }
     else {
-      this.setState({ currentCount: this.state.currentCount + 1 });
+        this.setState({
+            currentCount: this.state.currentCount + 1,
+            leftPartOfWord: leftPartWithoutHighlightedLetter,
+            rightPartOfWord: rightPart,
+            highlightedLetter: highlightedLetter
+        });
     }
   }
 
   render() {
     return (
-      <View>
-        <Text
-          style={{
-            fontSize: 0.8,
-            fontWeight: "400",
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: "center",
-            textAlignVertical: "center",
-            transform: [{ translate: [0, 0, -3] }]
-          }}
-        >
-          {textSplit[this.state.currentCount]}
-        </Text>
-      </View>
+        <View>
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            width: 2,
+            alignItems: 'stretch',
+            transform: [{ translate: [-1, 1, -5] }],
+        }}>
+            <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red' }}>
+                <Text style={{ fontSize: 0.2, textAlign: 'center' }}>{this.state.leftPartOfWord}</Text>
+            </View>
+            <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange' }}>
+                <Text style={{ fontSize: 0.2, textAlign: 'center' }}>{this.state.highlightedLetter}</Text>
+            </View>
+            <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red' }}>
+                <Text style={{ fontSize: 0.2, textAlign: 'center' }}>{this.state.rightPartOfWord}</Text>
+            </View>
+        </View>
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            width: 2,
+            alignItems: 'stretch',
+            transform: [{ translate: [-1, 1, -5] }],
+        }}>
+            <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red' }}>
+                <Text style={{ fontSize: 0.2, textAlign: 'center' }}>{this.state.leftPartOfWord}</Text>
+            </View>
+        </View>
+        <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            width: 2,
+            alignItems: 'stretch',
+            transform: [{ translate: [-1, 1, -5] }],
+        }}>
+            <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red' }}>
+                <Text style={{ fontSize: 0.2, textAlign: 'center' }}>{this.state.leftPartOfWord}</Text>
+            </View>
+        </View>
+        </View>
     );
   }
 }
